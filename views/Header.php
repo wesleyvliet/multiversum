a11f7d4702221d5b5eddaef70103b487b9a99471
wesleyvliet <507503@edu.rocmn.nl>
2020-02-21 11:28:53 +0100

    Update Header.php

--- views/Header.php
+++ views/Header.php
@@ -7,4 +7,10 @@
     </head>
 	<body>
     <nav>
+        <ul>
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
     </nav>
