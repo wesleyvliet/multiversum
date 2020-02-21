08a0423fc8e11fbc8f8abb0362b4e7949d0cce75
Bas Visser <bassebeessie@hotmail.com>
2020-02-21 13:58:51 +0100

    Update Contact.php

--- views/Contact.php
+++ views/Contact.php
@@ -1,1 +1,20 @@
-
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <h2 id="title">Contactgegevens</h2>
+    <p id="info1">
+    Gegevens eigenaar: <br>
+    Eigenaar: Jack Jones <br>
+    email: jack.jones@multiversum.com
+    </p>
+    <p id="info2">
+    Gegevens eigenaar:
+    Eigenaar: Jack Jones
+    email: jack.jones@multiversum.com
+    </p>
+</body>
+</html>
