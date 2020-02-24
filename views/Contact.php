56dbec1dd69170f5cbe671ca9eee68ee51f2f994
wesleyvliet <507503@edu.rocmn.nl>
2020-02-24 12:51:31 +0100

    tips bas :)

--- views/Contact.php
+++ views/Contact.php
@@ -1,20 +1,18 @@
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-  <h2 id="title">Contactgegevens</h2>
-    <p id="info1">
-    Gegevens eigenaar: <br>
-    Eigenaar: Jack Jones <br>
-    email: jack.jones@multiversum.com
-    </p>
-    <p id="info2">
-    Gegevens eigenaar:
-    Eigenaar: Jack Jones
-    email: jack.jones@multiversum.com
-    </p>
-</body>
-</html>
+    <!---
+    in views moet je geen header, html en body tags gebruiken. die worden
+    geopend in Header.php en gesloten in de Footer.php :)
+    ps: bekijk home page voor die banner om titles te gerbuiken.
+    --->
+    <div class="wrapper">
+        <h2 id="title">Contactgegevens</h2>
+        <p id="info1">
+        Gegevens eigenaar: <br>
+        Eigenaar: Jack Jones <br>
+        email: jack.jones@multiversum.com
+        </p>
+        <p id="info2">
+        Gegevens eigenaar:
+        Eigenaar: Jack Jones
+        email: jack.jones@multiversum.com
+        </p>
+    </div>
