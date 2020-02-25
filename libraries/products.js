be5d13bfa003002f050ad05dba9ebd1b370ad91e
wesleyvliet <507503@edu.rocmn.nl>
2020-02-25 10:55:41 +0100

    products v0.2

--- libraries/products.js
+++ libraries/products.js
@@ -12,8 +12,10 @@ function init_display() {
         3: {"title": 'Oculus Rift S', "platform": 'PC', "resulatie": '2560x1440 (Quad HD)'},
         4: {"title": 'Oculus Rift S', "platform": 'PC', "resulatie": '2560x1440 (Quad HD)'},
         5: {"title": 'Oculus Rift S', "platform": 'PC', "resulatie": '2560x1440 (Quad HD)'},
-        6: {"title": 'Oculus Rift S', "platform": 'PC', "resulatie": '2560x1440 (Quad HD)'},
-        7: {"title": 'Oculus Rift S', "platform": 'PC', "resulatie": '2560x1440 (Quad HD)'}
+		6: {"title": 'Oculus Rift S', "platform": 'PC', "resulatie": '2560x1440 (Quad HD)'},
+		7: {"title": 'Oculus Rift S', "platform": 'PC', "resulatie": '2560x1440 (Quad HD)'},
+        8: {"title": 'Oculus Rift S', "platform": 'PC', "resulatie": '2560x1440 (Quad HD)'},
+        9: {"title": 'Oculus Rift S', "platform": 'PC', "resulatie": '2560x1440 (Quad HD)'}
     };
     function display(arr) {
         var results = arr;
@@ -22,8 +24,8 @@ function init_display() {
             html += "<div class='item'>";
 				html += "<h1>" + results[i]['title'] + "</h1>";
 				html += "<img src='http://localhost/multiversum/multiversum/libraries/img/header4.jpeg'>";
-				html += "<p>" + results[i]['platform'] + "</p>";
-				html += "<p>" + results[i]['resulatie'] + "</p>";
+				html += "<p>Platform: " + results[i]['platform'] + "</p>";
+				html += "<p>Resulatie: " + results[i]['resulatie'] + "</p>";
 			html += "</div>";
         }
 		$('#products').html(html);
