737ebc85ee2c4a8cfd26862920d8801aa387d3d1
wesleyvliet <507503@edu.rocmn.nl>
2020-02-29 12:51:24 +0100

    product menu v0.1

--- views/Home.php
+++ views/Home.php
@@ -4,4 +4,9 @@
         <p>use this for text section :)</p>
     </span>
     <div id='products'></div>
+    <div class="buttons">
+        <input type="button" value="<">
+        <input value="1">
+        <input type="button" value=">">
+    </div>
 </div>
