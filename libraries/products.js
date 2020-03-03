6416968ca4d469a0ff414ce575ad607a9ac13188
wesleyvliet <507503@edu.rocmn.nl>
2020-03-03 15:16:14 +0100

    product details done

--- libraries/products.js
+++ libraries/products.js
@@ -119,12 +119,42 @@ function init_display() {
 		html +=				"<tr><th style='background: var(--grey);'>Platform</th></tr>";
 		html += 			"<tr><td>Platform (VR-brillen):</td></tr>";
 		html +=			"</table>";
+		html +=			'<table class="detail-table">';
+		html +=				"<tr><th style='background: var(--grey);'>Scherm</th></tr>";
+		html += 			"<tr><td>Eigen display:</td></tr>";
+		html += 			"<tr><td>Resolutie:</td></tr>";
+		html += 			"<tr><td>Refresh rate:</td></tr>";
+		html +=			"</table>";
+		html +=			'<table class="detail-table">';
+		html +=				"<tr><th style='background: var(--grey);'>Functies</th></tr>";
+		html += 			"<tr><td>Functies (VR-bril):</td></tr>";
+		html +=			"</table>";
+		html +=			'<table class="detail-table">';
+		html +=				"<tr><th style='background: var(--grey);'>Verbindingen</th></tr>";
+		html += 			"<tr><td>Aansluitingen VR-bril:</td></tr>";
+		html +=			"</table>";
 		html +=		"</div>";
 
 		html += 	"<div>";
 		html += 		"<h1>" + " Prijs €615,00 " + "<button>" + "Koop Nu!" + "</button></h1>";
 		html += 		"<div class='DetailsImgOverlay' style='background-image: linear-gradient(180deg,rgba(0,0,0,0.3) 0%,rgba(0,0,0,0.3) 100%), url(../multiversum/libraries/img/header1.jpeg)'>";
 		html +=			"</div>";
+		html +=			'<table class="detail-table">';
+		html +=				"<tr><th style='background: var(--grey);'>Accu en accessories</th></tr>";
+		html += 			"<tr><td>Meegeleverde VR-bril accessoires:</td></tr>";
+		html +=			"</table>";
+		html +=			'<table class="detail-table">';
+		html +=				"<tr><th style='background: var(--grey);'>Garantie</th></tr>";
+		html += 			"<tr><td>Fabrieksgarantie:</td></tr>";
+		html +=			"</table>";
+		html +=			'<table class="detail-table">';
+		html +=				"<tr><th style='background: var(--grey);'>Meer informatie</th></tr>";
+		html += 			"<tr><td>Product:</td></tr>";
+		html += 			"<tr><td>Merk:</td></tr>";
+		html += 			"<tr><td>Tweakers ID:</td></tr>";
+		html += 			"<tr><td>EAN:</td></tr>";
+		html += 			"<tr><td>SKU:</td></tr>";
+		html +=			"</table>";
 		html +=		"</div>";
 
 		html += "</div>"
