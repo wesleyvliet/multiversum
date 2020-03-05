3ce34b72cbab2a66765089945bcf6e97516a8b76
StrandedHobo <haitambacha0903@hotmail.com>
2020-03-05 15:03:57 +0100

    Form 1.1
    Form + Styling.
    
    Styling is in views\css\products.css onder de class addForm

--- views/Products.php
+++ views/Products.php
@@ -1,18 +1,21 @@
+
     <span>
         <h1>Toevoegen</h1>
-        <form class="item" action="index.html" method="post">
-            <label for="productName">Product naam</label> <br>
-            <input type="text" name="productName" value=""><br>
-            <label for="productPlatform">Product platform</label> <br>
-            <input type="text" name="productPlatform" value=""><br>
-            <label for="productResolution">Product resolutie</label><br>
-            <input type="text" name="productResolution" value=""><br>
-            <label for="productRefreshRate">Product refresh rate</label><br>
-            <input type="text" name="productRefreshRate" value=""><br>
-            <label for="productAccessories">Product accessoires</label><br>
-            <input type="text" name="productAccessories" value=""><br>
-            <input type="submit" name="" value="Toevoegen">
-        </form>
+        <div class="addForm">
+            <form class="addForm" action="index.html" method="post">
+                <label for="productName">Product naam</label> <br>
+                <input type="text" name="productName" value=""><br>
+                <label for="productPlatform">Product platform</label> <br>
+                <input type="text" name="productPlatform" value=""><br>
+                <label for="productResolution">Product resolutie</label><br>
+                <input type="text" name="productResolution" value=""><br>
+                <label for="productRefreshRate">Product refresh rate</label><br>
+                <input type="text" name="productRefreshRate" value=""><br>
+                <label for="productAccessories">Product accessoires</label><br>
+                <input type="text" name="productAccessories" value=""><br>
+                <input type="submit" name="" value="Toevoegen">
+            </form>
+        </div>
     </span>
     <footer>
         <p>© 1999-2020 multiversum.com b.v.</p>
