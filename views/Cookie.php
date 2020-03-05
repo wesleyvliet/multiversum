f6e769ad3a79808d728d164f999f4bfa152f6774
Bas Visser <44060243+babavisser@users.noreply.github.com>
2020-03-05 18:55:20 +0100

    Create Cookie.php

---
+++ views/Cookie.php
@@ -0,0 +1,31 @@
+<span>
+        <h1 id="title">Contactgegevens</h1>
+        <div class="grid-container">
+        <div class="item1">Gegevens eigenaar</div>
+        <div class="item2"> locatie:</div>
+        <div class="item3">Eigenaar: Jack Jones,</div>
+        <div class="item4">Straat: 1861 Jan Pieterszoon Coenstraat,</div>
+        <div class="item5">email:<a href="mailto:jack.jones@multiversum.com">jack.jones@multiversum.com</a></div>
+        <div class="item6">City: Maasdriel,</div>
+        <div class="item7"></div>
+        <div class="item8"> Stad: Zeeland,</div>
+            <div class="item7"></div>
+            <a href="https://www.google.nl/maps/place/Jan+Pieterszoon+Coenstraat,+Utrecht/@52.0910521,5.0953237,17z/data=!3m1!4b1!4m5!3m4!1s0x47c66f65451cd7b3:0xa52fd8e5ccf60705!8m2!3d52.0910488!4d5.0975124" target="_blank">
+        <div class="item8"> Postcode: 69217</div></a>
+        </div>
+    </div>
+    </span>
+    <a href="https://www.google.nl/maps/place/Jan+Pieterszoon+Coenstraat,+Utrecht/@52.0910521,5.0953237,17z/data=!3m1!4b1!4m5!3m4!1s0x47c66f65451cd7b3:0xa52fd8e5ccf60705!8m2!3d52.0910488!4d5.0975124" target="_blank">
+        <img src="./libraries/img/location.png" class="center" alt="locatie">
+    </a>
+    </body>
+    </span>
+    <footer>
+        <a href="Algemene.php">Algemene voorwaarden</a>
+        <a href="Privacy.php">Privacy</a>
+        <a href="Cookies.php">Cookies</a>
+        <p>© 1999-2020 multiversum.com b.v.</p>
+    </footer>
+    
+
+</html>
