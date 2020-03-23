a7ae83944fb6ce1c9872269140c17bdef65c9f80
Bas Visser <44060243+babavisser@users.noreply.github.com>
2020-03-23 11:28:31 +0100

    Update contact.php

--- view/contact.php
+++ view/contact.php
@@ -7,6 +7,14 @@ include 'header.php';
 </head>
 <body>
 <h1 id=title>Contactgegevens</h1>
+<p>Send E-mail</p>
+<form action="contactform.php" method="post">
+  <input type="text" name="name" placeholder="Volledige naam">
+  <input type="text" name="mail" placeholder="Jouw e-mail">
+  <input type="text" name="subject" placeholder="Onderwerp">
+  <textarea name="message" placeholder="Bericht"></textarea>
+  <button type="submit" name="submit">Stuur Mail</button>
+</form>
 </body>
 </html>
 <?php
