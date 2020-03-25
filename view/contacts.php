5647e18a31bb845cc2e91fc0c4665a14410a6fea
StrandedHobo <haitambacha0903@hotmail.com>
2020-03-25 13:16:34 +0100

    Werkende fotos
    fotos werken

--- view/contacts.php
+++ view/contacts.php
@@ -10,7 +10,7 @@ if(!empty($actions)) {
         $discount = $discount - intval($actions[$i]['korting']);
         $html .= "<div class='item' id=" . $actions[$i]['id'] . ">";
             $html .= "<h1>" . $actions[$i]['title'] . '</h1><h1><p style="margin: 0; float: left;">€ '. $discount . ' </p><p style="margin: 0; font-size: 14px; padding-bottom: 10px; color: var(--green);">-' . $actions[$i]['korting'] . "</p></h1>";
-            $html .= "<img src='view/assets/img/products/" . $actions[$i]['id'] . ".jpeg'>";
+            $html .= "<img src='view/assets/img/products/" . $actions[$i]['id'] . ".jpg'>";
             $html .= "<p>Platform: " . $actions[$i]['platform'] . "</p>";
             $html .= "<p>Resulatie: " . $actions[$i]['resulatie'] . "</p>";
         $html .= "</div>";
@@ -34,7 +34,7 @@ $html = "<div id='products' class='product-display' display='1' >";
 for($i = 0; $i < 10; $i++){
     $html .= "<div class='item' id=" . $display[$i]['id'] . ">";
         $html .= "<h1>" . $display[$i]['title'] . '<h1>€ '. $display[$i]['prijs'] . "</h1></h1>";
-        $html .= "<img src='view/assets/img/products/" . $display[$i]['id'] . ".jpeg'>";
+        $html .= "<img src='view/assets/img/products/" . $display[$i]['id'] . ".jpg'>";
         $html .= "<p>Platform: " . $display[$i]['platform'] . "</p>";
         $html .= "<p>Resulatie: " . $display[$i]['resulatie'] . "</p>";
     $html .= "</div>";
