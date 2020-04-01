fb09774f99f15957fa35c6371bcd66c1fc70eb33
wesleyvliet <60602851+wesleyvliet@users.noreply.github.com>
2020-04-01 14:39:16 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
-    if ((function_exists('session_status') && session_status() !== PHP_SESSION_ACTIVE) || !session_id()) {
-        session_start();
-        $_SESSION['name'] = 'guest';
-        $_SESSION['pass']   = 'guest';
+session_start();
+
+define('PATH', getcwd());
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
+spl_autoload_register(function($class_name) {
+    // die('/classes/'.$class_name.'.php');
+    if (file_exists(PATH.'/classes/'.$class_name.'.php')) {
+        require_once  PATH."/classes/{$class_name}.php";
+    } else if (file_exists(PATH.'/Controllers/'.$class_name.'.php')) {
+        require_once  PATH."/Controllers/{$class_name}.php";
     }
-    require_once 'controller/ContactsController.php';
-    $controller = new ContactsController();
-    $controller->handleRequest();
- ?>
+});
+
+require_once 'controllers/mainController.php';
+
+$controller = new ContactsController();
+$controller->handleRequest();
+
+?>
