7e866fb73ff344b7223414749bad0cc4a6c283a8
Bas Visser <44060243+babavisser@users.noreply.github.com>
2020-04-08 12:18:02 +0200

    Update contact.php

--- views/contact.php
+++ views/contact.php
@@ -13,7 +13,7 @@ echo $html;
     <div class="contact-grid">
         <div>
             <h1>Waar wij gevestigd zijn.</h1>
-            <div class='contact-location'></div>
+            <div class='contact-location'><a href="https://www.google.nl/maps/place/Jan+Pieterszoon+Coenstraat,+Utrecht/@52.0912708,5.0973306,17z/data=!4m5!3m4!1s0x47c66f65451cd7b3:0xa52fd8e5ccf60705!8m2!3d52.0910488!4d5.0975124" target="_blank">link</a></div>
         </div>
         <div>
             <h1>alle velden met * is verplicht.</h1>
